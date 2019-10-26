8cc22fd183824784d12e47ff5db793f76077df07
Carlos Eduardo <duducp2013@gmail.com>
2019-10-26 07:43:18 -0300

    Adicionada rota para registro de cliente

--- app/routes/auth_route.py
+++ app/routes/auth_route.py
@@ -1,8 +1,10 @@
 from flask_restplus import Resource
 
 from app.handlers.auth_handler import AuthHandler
+from app.handlers.client_handler import ClientHandler
 from app.restplus import api
 from app.schemas.routes.auth_schema import AuthSchemaRoute
+from app.schemas.routes.client_schema import ClientSchemaRoute
 
 ns = api.namespace(
     path="/auth",
@@ -11,6 +13,7 @@ ns = api.namespace(
 )
 
 schema = AuthSchemaRoute()
+schema_client = ClientSchemaRoute()
 
 
 @ns.route("/login")
@@ -29,6 +32,24 @@ class Login(Resource):
         Log in to the system
         """
         return self.auth.login()
+
+
+@ns.route("/register")
+@api.doc(security=None)
+@api.response(code=500, description="internal_error")
+@api.response(code=401, description="unauthorized")
+class Register(Resource):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.auth = ClientHandler()
+
+    @api.response(code=201, model=schema_client.response_client, description="success")
+    @api.doc(security=False, body=schema_client.client_item)
+    def post(self):
+        """
+        Register client
+        """
+        return self.handler.post()
 
 
 @ns.route("/logout")
